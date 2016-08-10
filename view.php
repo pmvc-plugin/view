9108ea698d9cd54e0c8940ac7bfd87c1c87ebb92
Hill <hill@kimo.com>
2016-08-10 09:51:49 +0000

    Add change function for viewengine

--- view.php
+++ view.php
@@ -2,13 +2,31 @@
 namespace PMVC\PlugIn\view;
 
 use PMVC as p;
+use SplSubject;
+use DomainException;
 
 ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\view';
 
 class view extends p\PlugIn
 {
-    public function update(\SplSubject $SplSubject=null)
+
+    private $_view;
+
+    public function init()
     {
-        return p\plug('view_'.p\getOption(_VIEW_ENGINE));
+        $this->change(p\getOption(_VIEW_ENGINE));
+    }
+
+    public function change($viewEngine)
+    {
+        $this->_view = 'view_'.$viewEngine;
+        if (!p\plug($this->_view, [\PMVC\PAUSE=>true])) {
+           throw new DomainException('View engine not eixis.['.$this->_view.']'); 
+        }
+    }
+
+    public function update(SplSubject $SplSubject=null)
+    {
+        return p\plug($this->_view);
     }
 }
